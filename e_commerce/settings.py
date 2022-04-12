10ad8935eb96bc4b3129bfdc4e17976410dd45be
Jonathans Schaffer <jonathans@lumnieducacao.com.br>
2022-04-12 12:12:10 -0300

    Criar app Users

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # meus apps
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# User Model
+
+AUTH_USER_MODEL = "users.User"
